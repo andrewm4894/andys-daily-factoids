2c95f42b12f9c3215608a5b0d88e706e052b212d
andrewmaguire-stripe <andrewmaguire@stripe.com>
2024-12-18 23:49:21 +0000

    Update FactoidCard.js

--- frontend/src/components/FactoidCard.js
+++ frontend/src/components/FactoidCard.js
@@ -15,10 +15,10 @@ function FactoidCard({ factoid, onVote }) {
     <div className="factoid-card">
       <p className="factoid-text">{factoid.text}</p>
       <div className="meta">
-        <button className="vote-button upvote" onClick={() => onVote(factoid.id, 'up')}>👍 <span className="votes">{factoid.votesUp}</span></button>
-        <button className="vote-button downvote" onClick={() => onVote(factoid.id, 'down')}>👎 <span className="votes">{factoid.votesDown}</span></button>
-        <button onClick={handleGoogleSearch} className="google-button">Google This!</button>
-        <span className="created-at">{new Date(factoid.createdAt._seconds * 1000).toLocaleString()}</span>
+        <button className="vote-button upvote" onClick={() => onVote(factoid.id, 'up')} title="My mind is blown!">🤯 <span className="votes">{factoid.votesUp}</span></button>
+        <button className="vote-button downvote" onClick={() => onVote(factoid.id, 'down')} title="Meh">😒 <span className="votes">{factoid.votesDown}</span></button>
+        <button onClick={handleGoogleSearch} className="google-button" title="Search up that bad doy">Google!</button>
+        <span className="created-at" title="Created At">{new Date(factoid.createdAt._seconds * 1000).toLocaleString()}</span>
       </div>
     </div>
   );
